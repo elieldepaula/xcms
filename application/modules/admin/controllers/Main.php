98cbe0a75ece237f3cc6dbff7c8f0b3f481e6ae4
Eliel de Paula <elieldepaula@gmail.com>
2016-11-28 11:19:30 -0200

    Redefinição da classe MY_Controller

--- application/modules/admin/controllers/Main.php
+++ application/modules/admin/controllers/Main.php
@@ -1,34 +1,18 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Main extends MY_Controller {
+class Main extends Authenticated_Controller {
 
 	function __construct()
 	{
 		parent::__construct();
 
-		if (!$this->ion_auth->logged_in())
-		{
-			// redirect them to the login page
-			//redirect('admin/logon/login', 'refresh');
+		if (!$this->ion_auth->is_admin()){
+			$this->session->set_flashdata('after_login', current_url());
+            redirect('/login');	
 		}
+
 	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 
